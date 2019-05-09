4cd04238ee02e53f6e4bfdcc45d95832305d05ff
3anovproduction <50446215+3anovproduction@users.noreply.github.com>
2019-05-09 22:38:10 +0700

    Update novtools.sh

--- novtools.sh
+++ novtools.sh
@@ -15,21 +15,12 @@ echo
 echo "Selamat Datang, Nama Anda Siapa?"
 read -p "Masukkan Nama Anda : " nama
 echo
-read -p "Hai $nama, Semoga Kabarmu Baik-Baik Saja <enter>"
-sleep 0.3
-echo
-load(){
-    echo -e "\n"
-    bar=" ==================================================== "
-    barlength=${#bar}
-    i=0
-    while((i<100)); do
-        n=$((i*barlength / 100))
-        printf "\e[00;32m\r[%-${barlength}s]\e[00m" "${bar:0:n}"
-        ((i += RANDOM%5+2))
-        sleep 0.2
-    done
+read -p "Hai $nama, Ingin Lanjut?"
+echo "yes/no"
+read confirm
+if [ $confirm = "yes" ]; then
 clear
+sleep 0.3
 echo "[#]> Tools Sudah Di Aktifkan" | lolcat
 echo "[#]> Exit 00" | lolcat
 sleep 1
