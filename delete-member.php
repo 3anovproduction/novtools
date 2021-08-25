28fbe67ce4f1f2320112bcfeb60db6e86e94070b
Trianov Widyandaru <50446215+3anovproduction@users.noreply.github.com>
2021-08-25 11:15:08 +0700

    Delete delete-member.php

--- delete-member.php
+++
@@ -1,7 +0,0 @@
-<?php
-    include 'koneksi.php';
-    if(isset($_GET['user_id'])){
-        $delete = mysqli_query($conn, "DELETE FROM tb_pendaftaran WHERE user_id = '".$_GET['user_id']."'");
-        echo '<script>window.location ="member.php"</script>';
-    }
-?>
